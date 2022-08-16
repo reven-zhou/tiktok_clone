ef19d403783fa42fe5c90dcbeb40cb4f0db0daff
reven <3393037864@qq.com>
2022-08-16 19:34:56 +0800

    first commit

---
+++ cmps/NoResults.tsx
@@ -0,0 +1,22 @@
+import React from "react";
+import { MdOutlineVideocamOff } from "react-icons/md";
+import { BiCommentX } from "react-icons/bi";
+import { NextPage } from "next";
+import { Video } from "../types";
+
+interface IProps {
+  text: string;
+}
+
+const NoResults: NextPage<IProps> = ({ text }) => {
+  return (
+    <div className='flex flex-col justify-center items-center h-full w-full'>
+      <p className='text-8xl'>
+        <MdOutlineVideocamOff />
+      </p>
+      <p className='text-2xl text-center'>{text}</p>
+    </div>
+  );
+};
+
+export default NoResults;
